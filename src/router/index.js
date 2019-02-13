9f3a517591a6df31b4dfd69d7641929b7b7570c2
chen <1019115621@qq.com>
2019-02-13 17:08:18 +0800

    登录组件的布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login.vue'
+import Home from '@/components/home.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld'
-    }
-  ]
+  routes: [{
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: Login
+  }]
 })
